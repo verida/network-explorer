b0c0e4d183ef7bf71fccf22b4108d877029a3dad
chidodev <chidev0104@gmail.com>
2024-06-17 18:03:57 -0700

    fix: typo error

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -84,7 +84,7 @@ const Navbar = () => {
           </DropdownMenu>
           <Link href="/">
             <img
-              src="/logo.png"
+              src="/logo.svg"
               className="lg:w-[100px] w-[70px] cursor-pointer"
               alt="logo"
             />
