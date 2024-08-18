32649f9a0d8c54c445beef3163ee371788e42b82
aurelticot <aurelticot.dev@gmail.com>
2024-08-18 18:53:54 +1000

    feat: display id on Node and identity pages

--- src/app/identities/[did]/layout.tsx
+++ src/app/identities/[did]/layout.tsx
@@ -4,10 +4,19 @@ import { PageLayoutWrapper } from "@/components/common/page-layout-wrapper"
 
 type IdentityLayoutProps = {
   children: React.ReactNode
+  params: {
+    did: string
+  }
 }
 
 export default function IdentityLayout(props: IdentityLayoutProps) {
-  const { children } = props
+  const { children, params } = props
+  const { did: encodedDid } = params
+  const did = decodeURIComponent(encodedDid)
 
-  return <PageLayoutWrapper title="Identity">{children}</PageLayoutWrapper>
+  return (
+    <PageLayoutWrapper title="Identity" titleComplement={did}>
+      {children}
+    </PageLayoutWrapper>
+  )
 }
