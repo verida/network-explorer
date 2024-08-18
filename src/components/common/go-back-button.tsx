1c81b44c8f0edd33e6d5dbc7348c0d661bef8061
aurelticot <aurelticot.dev@gmail.com>
2024-08-18 18:47:13 +1000

    feat: update common components

--- src/components/common/go-back-button.tsx
+++ src/components/common/go-back-button.tsx
@@ -5,8 +5,16 @@ import React, { useCallback } from "react"
 import { FaChevronLeft } from "react-icons/fa"
 
 import { Button } from "@/components/ui/button"
+import { cn } from "@/styles/utils"
 
-export function GoBackButton() {
+export type GoBackButtonProps = Omit<
+  React.ComponentProps<typeof Button>,
+  "children" | "onClick"
+>
+
+export function GoBackButton(props: GoBackButtonProps) {
+  const { variant = "ghost", size = "icon", className, ...buttonProps } = props
+
   const router = useRouter()
 
   const handleClick = useCallback(() => {
@@ -14,7 +22,13 @@ export function GoBackButton() {
   }, [router])
 
   return (
-    <Button variant="ghost" size="icon" onClick={handleClick}>
+    <Button
+      variant={variant}
+      size={size}
+      onClick={handleClick}
+      className={cn(className)}
+      {...buttonProps}
+    >
       <FaChevronLeft className="h-6 w-6 text-muted-foreground" />
       <span className="sr-only">Go back</span>
     </Button>
