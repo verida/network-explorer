cee8ea2c48eb500aede8028e3d7870ebd75ef925
aurelticot <aurelticot.dev@gmail.com>
2024-09-23 18:52:20 +1000

    refactor: split error and not found component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link"
 import React from "react"
 
-import { ErrorPageContent } from "@/components/common/error-page-content"
+import { Button } from "@/components/ui/button"
 import { getRootPageRoute } from "@/features/routes/utils"
 
 export default function RootNotFoundPage() {
   return (
-    <ErrorPageContent
-      message="This page doesn't exist"
-      buttonHref={getRootPageRoute()}
-      buttonLabel="Return to the home page"
-    />
+    <div className="flex h-full flex-row items-center justify-center">
+      <div className="flex flex-col items-center gap-8">
+        <p>{`This page doesn't exist`}</p>
+        <Button asChild className="w-fit">
+          <Link href={getRootPageRoute()}>Return to the home page</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
